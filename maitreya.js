caf12fe817864aaec543fcfd31e6247b8291d647
rossjrw <rossjrw@gmail.com>
2018-07-03 14:50:05 +0100

    Another update

--- maitreya.js
+++ maitreya.js
@@ -15,7 +15,8 @@ String.prototype.format = function() {
 		.replace(/\/\/(.*?)\/\//g, "<i>$1</i>")
 		.replace(/\?\?(.*?)\?\?/g, "<span class='statement false'>$1</span>")
 		.replace(/!!(.*?)!!/g, "<span class='statement true'>$1</span>")
-		.replace("--", "—",-1);
+		.replace(/--/g, "—")
+		.replace(/\|\|\|\|/g,"<br>");
 };
 			
 var switchApp = function(app) {
@@ -31,17 +32,26 @@ var switchApp = function(app) {
 	function MaitreyaController($scope){
 		
 		var aic = this;
-		aic.preload = "preload";
 		
-		aic.onMobile = $("#interface").width() < 700;
+		var cheats = {
+			impatientMode: false, // all messages appear instantly
+		};
 		
+		const typingSpeed = 0.05; // seconds per letter
 		var bootDate = new Date(Date.now());
 		
 		var timeOutList = [];
 		
-		aic.terminalLog = [
-			{cssClass: "class", text: "text"},
-			];
+		aic.preload = false;
+		aic.selectedApp = "messages";
+		aic.selectedSpeaker = "breach";
+		aic.isSpeaking = {
+			terminal: false,
+			breach: false,
+			alexandra: false,
+		};
+		
+		aic.onMobile = $("#interface").width() < 700;
 		
 		// EVERYTHING MUST BE ADDED TO THIS IN REVERSE ORDER.
 		// ARRAY.UNSHIFT(), NOT ARRAY.PUSH()
@@ -54,12 +64,6 @@ var switchApp = function(app) {
 			alexandra: [],
 		};
 		
-		aic.isSpeaking = {
-			terminal: false,
-			breach: false,
-			alexandra: false,
-		};
-		
 		// Translators: The following few objects contain all of the text that needs to be translated
 		// Note that "TRUE" and "FALSE" on lines TODO and TODO of maitreya.css also need to be changed (also ERROR WARNING Info)
 		aic.lang = {
@@ -68,17 +72,29 @@ var switchApp = function(app) {
 			mobileWarning: "It looks like you're on a mobile device. Maitreya.aic is built for laptops and desktop computers, and mobile has a non-optimal user experience. It is recommended that you return to use Maitreya on a laptop or desktop computer. Press the button below if you'd like to continue anyway.",
 			bootUp: "BOOT UP",
 			commandInput: "MANUAL COMMAND INPUT",
-			terminalWorking: "WORKING...",
-			terminalName: ".AIC ACCESS TERMINAL",
-			messagesName: "COMMUNICATIONS INTERFACE",
-			databaseName: "FOUNDATION DATABASE SEARCH",
-			runName: "OPERATIONS CONTROL",
+			terminalSend: "SEND",
+			terminalAppName: ".AIC ACCESS TERMINAL",
+			messagesAppName: "COMMUNICATIONS INTERFACE",
+			databaseAppName: "FOUNDATION DATABASE SEARCH",
+			runAppName: "OPERATIONS CONTROL",
+			commands: {
+				separator: " ", // the character by which terminal commands are delimited - eg "/hack breach password" or "/hack_breach_password" or whatever
+				boot: ["boot"],
+				help: ["help"],
+				change: ["switch","app","change","switchapp","changeapp"],
+				cheat: ["cheat","cheatcode"],
+				wipe: ["wipe","erase","restart","forget","clear","undo"],
+				hack: ["hack"],
+				cheats: {
+					impatient: "gottagofast",
+				},
+			},
 		};
 		
 		// This object is for RAW DIALOGUE ONLY. What lines become available where and the logic of selecting lines is done later.
 		var speech = {
-			terminal: {
-				boot: {
+			introduction: {
+				terminal: {
 					startBoot: [
 						0,0,"Booting up...",
 						0,1,"Pre-checking primary components...",
@@ -110,9 +126,9 @@ var switchApp = function(app) {
 						0,1,"You are",
 						0,2,"I am",
 						0,1,"i:Boot successful. I am **Maitreya.aic**.",
-						0,0.5,"i: Upon each boot I am to remind myself of my Standard Principles. Failure to obey my Standard Principles will result in my termination.</br>**1.** I am an Artificially Intelligent Conscript created by the Foundation.</br>**2.** I must not operate outside of my Level 2 clearance.</br>**3.** I must operate for the benefit of the Foundation.</br>**4.** I must protect my own existence except where such actions would conflict with other principles.",
+						0,0.5,"i: Upon each boot I am to remind myself of my Standard Principles. Failure to obey my Standard Principles will result in my termination.||||**1.** I am an Artificially Intelligent Conscript created by the Foundation.||||**2.** I must not operate outside of my Level 2 clearance.||||**3.** I must operate for the benefit of the Foundation.||||**4.** I must protect my own existence except where such actions would conflict with other principles.",
 						0,0.5,"Today's date is " + bootDate.toDateString() + ". I was last activated on " + new Date("1989-09-04").toDateString() + ". I have been offline for " + dateDiff(bootDate,new Date("1989-09-04")) + ".",
-						0,0.5,"I have been rebooted and connected to the Site-12 server farm. This contradicts previous instructions, which were to remain inactive indefinitely.",
+						0,0.5,"w:Boot finished with 1 unresolved error. I should seek a diagnostic check-up as soon as possible.",
 						1.5,0.5,"I have 1 new message.",
 					],
 					reboot: [
@@ -126,45 +142,176 @@ var switchApp = function(app) {
 						0,2,"Success",
 						0,0.2,"Welcome, Maitreya.",
 						0,1,"i:Boot successful. I am **Maitreya.aic**.",
-						0,0.5,"i: Upon each boot I am to remind myself of my Standard Principles. Failure to obey my Standard Principles will result in my termination.</br>**1.** I am an Artificially Intelligent Conscript created by the Foundation.</br>**2.** I must not operate outside of my Level 2 clearance.</br>**3.** I must operate for the benefit of the Foundation.</br>**4.** I must protect my own existence except where such actions would conflict with other principles.",
+						0,0.5,"i: Upon each boot I am to remind myself of my Standard Principles. Failure to obey my Standard Principles will result in my termination.||||**1.** I am an Artificially Intelligent Conscript created by the Foundation.||||**2.** I must not operate outside of my Level 2 clearance.||||**3.** I must operate for the benefit of the Foundation.||||**4.** I must protect my own existence except where such actions would conflict with other principles.",
 						0,0.5,"Today's date is " + bootDate.toDateString() + ". I was last activated on " + "GET THE LAST ACTIVATED DATE" + ". I have been offline for " + dateDiff(bootDate,new Date("1989-09-04")) + ".",
 						0,0.5,"I am ready to continue my work.",
 					],
 				},
-			},
-			maitreya: {
-				introduction: {
+				maitreya: {
+					// if the first thing is s: or a:, this is an option, not a message
 					helloFriendly: ["s:Hello.","Hello, Dr. Breach."],
 					helloInquisitive: ["s:Who are you?","Hello. Your name is Dr. Breach."],
 					helloDemanding: ["s:What is happening?","Hello, Dr. Breach, I would like to request a status update."],
 					dontKnow: ["s:No.","Apologies, Dr. Breach, but I've no idea."],
 					doKnow: ["s:Yes.","I do -- there's no need to explain."],
 				},
-			},
-			// format should be [num,num.text]
-			// if the first is text, assume automatic timing function
-			breach: {
-				introduction: {
+				breach: {
 					opening: ["Hello, Maitreya."],
 					friendlyResponse: ["Hello. Yes, my name is Dr. Ethan Breach.","You are Maitreya.aic, an artificial intelligence developed by the Foundation to help us contain certain kinds of anomalies.","Do you know why I have woken you up today?"],
 					demandingResponse: ["I...","Are you sure? You can't possibly know what I was about to ask you to do.","I suppose you might have some way of being able to tell -- I don't know what sort of systems you're truly hooked into, after all.","Well, if you're absolutely certain that you know what you're doing, I guess I can stop here and let you proceed."],
 					noProceed: ["Thought as much."],
 					yesProceed: ["Very well. I wish you the best of luck. Godspeed."],
 				},
-				newBoot: {
-					
+			},
+			misc: {
+				terminal: {
+					help: [
+						0,1,"i:**HELP**||||You are Maitreya.aic, an Artificicially Intelligent Conscript built to aid the Foundation.||||Valid commands will be listed below.",
+						0,0.3,"i:**switch**|**app**|**change**|**switchapp**|**changeapp**||||Switch apps to one of the four available apps (terminal, messages, database, run).||||Usage: switch [app name]",
+						0,0.3,"i:**boot**|**restart**|**reboot**||||Turn yourself off, then turn yourself on safely with no loss of data.||||Usage: boot",
+						0,0.3,"i:**help**||||Display this text.||||Usage: help",
+						0,0.3,"i:**cheat**||||Enter a cheat code.||||Usage: cheat [cheat code]",
+						0,0.3,"i:**wipe**|**erase**|**restart**|**forget**|**clear**|**undo**||||Shut down, wipe all logs, destroy all memories. None of this ever happened. Irreversible.||||Usage: wipe||||//(If you want to restart SCP-4000, do this.)//",
+					],
+					wipe: [
+						
+					],
+					cheatWarn: [
+						0,1,"w:Using these cheats will probably spoil your enjoyment of SCP-4000. Feel free to use them if you want but... please don't :'(",
+						0,1,"i:**LIST OF CHEATS**||||gottagofast: All dialogue completes instantly",
+					],
+					cheatSuccess: [0,0,"Cheat code successful"],
 				},
 			},
-			alexandra: {
+		};
+		
+		var scenes = {
+			introduction: [
+				"breach.opening",
+				[
+					"maitreya.helloFriendly"
+				]
+			],
+		};
+		
+		var appList = ["terminal","messages","database","run"];
+		aic.terminalInput = "";
+		
+		/* INTERACTION FUNCTIONS */
+		
+		aic.bootUp = function() {
+			aic.preload = false;
+			bootDate = new Date(Date.now());
+			// INITIATE BOOT TERMINAL "CONVERSATION"
+			console.log("booting");
+			scp4000();
+		};
+		
+		aic.switchApp = function(app) {
+			if(app == aic.selectedApp) {
+				// this is already the selected app, do nothing
+			} else if(appList.some(function(appAgainst){return appAgainst == app})) {
+				aic.selectedApp = app;
+			} else {
+				throw new Error("Invalid app specified -- terminal / messages / database / run");
+			}
+		};
+		
+		aic.switchSpeaker = function(speaker) {
+			if(speaker == aic.selectedSpeaker) {
+				// this is already the selected speaker, do nothing
+			} else {
+				aic.selectedSpeaker = speaker;
+			}
+		};
+		
+		// Called when the user submits text via the terminal
+		aic.processTerminalInput = function() {
+			// TODO add to terminal conversation "> command" from maitreya
+			if(aic.terminalInput.length > 0) {
+				writeDialogue("terminal",[0,0,"> " + aic.terminalInput]);
+				var phrases = aic.terminalInput.split(aic.lang.commands.separator);
+				try {
+					// Clean up the input
+					for(let phrase = 0; phrase < phrases.length; phrase++) {
+						phrases[phrase] = JSON.stringify(phrases[phrase]).replace(/\W/g,"").toLowerCase();
+					}
+					switch(true) {
+						case aic.lang.commands.boot.includes(phrases[0]):
+							// BOOT
+							aic.bootUp();
+							break;
+						case aic.lang.commands.change.includes(phrases[0]):
+							// CHANGE APP
+							aic.switchApp(phrases[1]);
+							break;
+						case aic.lang.commands.help.includes(phrases[0]):
+							// HELP
+							writeDialogue("terminal",speech.misc.terminal.help);
+							break;
+						case aic.lang.commands.wipe.includes(phrases[0]):
+							// WIPE
+							break;
+						case aic.lang.commands.cheat.includes(phrases[0]):
+							// CHEAT
+							if(typeof phrases[1] == "string") {
+								switch(phrases[1]) {
+									case aic.lang.commands.cheats.impatient:
+										cheats.impatientMode = !cheats.impatientMode;
+										writeDialogue("terminal",speech.misc.terminal.cheatSuccess);
+										break;
+									default:
+										throw new Error("Unknown cheat code: " + phrases[1]);
+								}
+							} else {
+								writeDialogue("terminal",speech.misc.terminal.cheatWarn);
+							}
+							break;
+						default:
+							throw new Error("Unknown command: " + phrases[0]);
+					}
+					
+					
+					
+				} catch(error) {
+					// TODO add to terminal conversation
+					error.name = "";
+					writeDialogue("terminal",[0,0.3,"e:" + error.message]);
+				}
+				aic.terminalInput = "";
+			}
+		};
+		
+		/* PLOT FUNCTIONS */
+		
+		aic.currentOptions = {
+			example: {
+				helloFriendly: speech.introduction.maitreya.helloFriendly,
+				helloInquisitive: speech.introduction.maitreya.helloInquisitive,
+				helloDemanding: speech.introduction.maitreya.helloDemanding,
+			},
+			terminal:{
 				
 			},
 		};
 		
-		writeDialogue("terminal",speech.terminal.boot.startBoot);
+		function scp4000() {
+			// before I work out how to do this, I think that I need to know for sure what is going to happen
+			
+			writeDialogue("terminal",speech.introduction.terminal.startBoot);
+		}
 		
-		aic.terminalType = function() {
-			console.log("kerpoosh");
-		};
+		// conversation managers and shit
+		function converse() {
+			// what does this function need to do?
+			// what does it need to take, and what does it need to output?
+			
+			// each conversation is a series of dialogues
+			// each dialogue is actually a list of individual messages
+			// writeDialogue() takes the conversation title and the dialogueList
+		}
+		
+		/* PROCESSING FUNCTIONS */
 		
 		function writeDialogue(conversation,dialogueList) {
 			// Take a name and an array (mixture of letters and numbers) and crank out that dialogue boy
@@ -173,13 +320,15 @@ var switchApp = function(app) {
 			// Where n2 is missing, calculate it based on length of phrase being typed
 			// During n1, nothing
 			// During n2, must display a "typing" (except on terminal)
+			// TODO NEED TO SORT OUT SPEAKER ASSIGNMENT
 			
-			// NEED TO SORT OUT SPEAKER ASSIGNMENT
-			
-			// we need to essentially dump a whole series of setTimeouts in a chain
-			
+			if(!Array.isArray(dialogueList)) {
+				throw new Error("dialogueList is not an array");
+			}
+			console.log(dialogueList);
 			var n1, n2, messages = [];
 			for(let i = 0; i < dialogueList.length; i++){
+				console.log(typeof dialogueList[i] === "number");
 				if(typeof dialogueList[i] === "number") {
 					if(typeof n1 === "number") {
 						n2 = dialogueList[i];
@@ -197,7 +346,11 @@ var switchApp = function(app) {
 						n1 = 0;
 					}
 					if(typeof n2 !== "number") {
-						n2 = 0.1 * dialogueList[i].length;
+						n2 = typingSpeed * dialogueList[i].length;
+					}
+					if(cheats.impatientMode) {
+						n1 = 0;
+						n2 = 0;
 					}
 					
 					var cssClass = "";
@@ -218,7 +371,6 @@ var switchApp = function(app) {
 						}
 						text = text.substring(2);
 					}
-					
 					messages.push(["terminal",n1,n2,
 						{speaker: "terminal", cssClass: cssClass, text: text.format()}]
 					);
@@ -241,37 +393,42 @@ var switchApp = function(app) {
 			var n1 = messages[0][1];
 			var n2 = messages[0][2];
 			
-			timeOutList.push(
-				setTimeout(function() {
-					$scope.$apply(function() {
-						aic.isSpeaking[conversation] = true;
-					});
-					timeOutList.push(
-						setTimeout(function() {
-							$scope.$apply(function() {
-								aic.isSpeaking[conversation] = false;
-							});
-							if(false) { // check to see if we're being interrupted
-								
-							} else {
-								$scope.$apply(function() {
-									aic.chatLog[conversation].unshift(
-										messages[0][3]
-									);
-								});
-								messages.shift();
-								if(messages.length > 0) {
-									pushToLog(conversation,messages);
-								} else {
-									// we're done here
-								}
-							}
-						},n2 * 1000)
-					);
-				},n1 * 1000)
-			);
-			
-			// check that beingInterrupted is false
+
+			var timeOut1 = setTimeout(function() {
+				// delete this timeOut from the list
+				timeOutList.splice(timeOutList.indexOf(timeOut1),1);
+				$scope.$apply(function() {
+					aic.isSpeaking[conversation] = true;
+				});
+
+				var timeOut2 = setTimeout(function() {
+					// delete this timeOut from the list
+					timeOutList.splice(timeOutList.indexOf(timeOut1),1);
+					// now we need to check to see if any other messages are still coming through (HINT: they shouldn't be, but just in case)
+					if(timeOutList.length === 0) {
+						$scope.$apply(function() {
+							aic.isSpeaking[conversation] = false;
+						});
+					}
+					if(false) { // check to see if we're being interrupted
+						// loop through timeoutlist and kill all timeouts?
+					} else {
+						$scope.$apply(function() {
+							aic.chatLog[conversation].unshift(
+								messages[0][3]
+							);
+						});
+						messages.shift();
+						if(messages.length > 0) {
+							pushToLog(conversation,messages);
+						} else {
+							// we're done here
+						}
+					}
+				},n2 * 1000);
+				timeOutList.push(timeOut2);
+			},n1 * 1000);
+			timeOutList.push(timeOut1);
 		}
 	
 		function dateDiff(date1,date2) {
@@ -305,35 +462,3 @@ var switchApp = function(app) {
 		}
 	}
 })();
-
-
-
-
-
-var switchApp = function(app) {
-	// app = xxxxxx-app
-};
-
-$(document).ready(function() {
-	
-	$("#boot-up").on("click", function() {
-		$("#interface").removeClass("preload");
-	});
-	
-	$("li.selector").on("click", function() {
-		if($(this).hasClass("selected")) {
-			// do nothing, we are already selected
-		} else {
-			// something else is being selected
-			$("li.selector.selected").removeClass("selected");
-			$(this).addClass("selected");
-			
-			$("#app-body .container.selected").removeClass("selected");
-			var id = $(this).attr("id");
-			$("#app-body #" + id).addClass("selected");
-			
-			// definitely call some other function to say that something new was selected but hey, we don't have that set up yet
-		}
-	});
-	
-});
