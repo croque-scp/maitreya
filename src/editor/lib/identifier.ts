0067a5c2b9abb7af98a7ab68ca49adfc50087bc3
Ross Williams <rossjrw@gmail.com>
2021-05-11 09:06:27 +0100

    Simply event extractor function

--- src/editor/lib/identifier.ts
+++ src/editor/lib/identifier.ts
@@ -1,131 +1,61 @@
-import {
-  Event,
-  eventOrInteractionIsEvent,
-  Identifier,
-  Interaction,
-} from "../types"
+import { Event, eventOrInteractionIsEvent, Identifier } from "../types"
 
-export function getInteractionWithIdentifier(
-  eventOrInteraction: Event | Interaction,
-  identifier: Identifier
-): Interaction | null {
-  return getEventOrInteractionWithIdentifier(
-    eventOrInteraction,
-    identifier,
-    "interaction"
-  )
-}
-
+/**
+ * From a registry of events, returns the event that matches the given
+ * identifier. Throws an error if no matching event is found.
+ *
+ * Returns the first result that matches. It is technically possible for
+ * there to be multiple matches, but there really shouldn't be.
+ *
+ * @param event - The root event that contains all the events to be
+ * searched. The first key of the identifier must match this event.
+ * @param identifier - The identifier for the event as an array of strings.
+ */
 export function getEventWithIdentifier(
   event: Event,
   identifier: Identifier
-): Event | null {
+): Event {
   console.log(
     "Getting event with id",
     JSON.stringify(identifier),
-    "in",
-    event.id
+    "in event with id",
+    JSON.stringify(event.id)
   )
-  const foundEvent = getEventOrInteractionWithIdentifier(
-    event,
-    identifier,
-    "event"
-  )
-  console.log("result:", foundEvent?.id ?? "XXX Did not find an event")
-  return foundEvent
-}
-
-/**
- * From a registry of events, returns the either an event or interaction that
- * matches the given identifier. Returns null if no such event or interaction
- * exists. Throws an error if the identifier does not point to the specified
- * type.
- *
- * Returns the first result that matches. It is possible for there to
- * be multiple matches, but other validation steps should prevent this.
- *
- * @param eventOrInteraction - The registry of events or the event that contains
- * all events to be searched.
- * @param identifier - The identifier for the event as an array of strings,
- * relative to the provided event.
- * @param searchType - The type of search to perform.
- */
-export function getEventOrInteractionWithIdentifier(
-  eventOrInteraction: Event | Interaction,
-  identifier: Identifier,
-  searchType: "event"
-): Event | null
-export function getEventOrInteractionWithIdentifier(
-  eventOrInteraction: Event | Interaction,
-  identifier: Identifier,
-  searchType: "interaction"
-): Interaction | null
-export function getEventOrInteractionWithIdentifier(
-  eventOrInteraction: Event | Interaction,
-  identifier: Identifier,
-  searchType: "event" | "interaction"
-): Event | Interaction | null {
-  // If there are no search terms in the identifier, the search is finished
-  if (identifier.length === 0) {
-    if (
-      eventOrInteractionIsEvent(eventOrInteraction) &&
-      searchType !== "event"
-    ) {
-      throw new Error("The identifier matches an event")
-    }
-    if (
-      !eventOrInteractionIsEvent(eventOrInteraction) &&
-      searchType === "event"
-    ) {
-      throw new Error("The identifier does not match an event")
-    }
-    return eventOrInteraction
-  }
-  if (
-    identifier.length === 1 &&
-    eventOrInteraction.id === identifier[0] &&
-    eventOrInteractionIsEvent(eventOrInteraction) &&
-    searchType === "event"
-  ) {
-    // Special case for matching the top-level event
-    // Special cases are probably bad ideas generally but w/e
-    console.log("Special case identifier search return for", identifier[0])
-    return eventOrInteraction
-  }
-  // If the result is an interaction, any deeper searching would match
-  // interaction internals
-  if (!eventOrInteractionIsEvent(eventOrInteraction)) {
-    throw new Error("The identifier is too deep to match an interaction")
-  }
-  // Check each of this event's interactions and sub-events for matches
-  // TODO Why is this checking the children of this event? Shouldn't it
-  //  check the current event first?
-  const match = eventOrInteraction.interactions.find(
-    (event) => event.id === identifier[0]
-  )
-  // If there is no match for the current search term, the interaction does
-  // not exist
-  if (!match) {
-    return null
-  }
-  // TypeScript will not infer the return type of the recursive call if the
-  // searchType is passed to it as a union type, so it has been manually
-  // split into its primitives
-  if (searchType === "event") {
-    return getEventOrInteractionWithIdentifier(
-      match,
-      identifier.slice(1),
-      searchType
+  // Check that the first term of the identifier matches this event
+  if (identifier[0] !== event.id) {
+    throw new Error(
+      `${identifier[0]} does not match ${JSON.stringify(event.id)}`
     )
   }
-  if (searchType === "interaction") {
-    return getEventOrInteractionWithIdentifier(
-      match,
-      identifier.slice(1),
-      searchType
-    )
+  // If that was the only (remaining) term in the identifier, the search is
+  // finished
+  if (identifier.length === 1) {
+    // All searches should end here - the remainder is used for recursion
+    console.log("Result:", JSON.stringify(event.id))
+    return event
   }
-  return null
+  // Find which if any of this event's children match the next term of the
+  // identifier
+  const children = event.interactions.map((child) => {
+    console.log("Event child:", JSON.stringify(child.id))
+    // Ensure that the child is an event, not an interaction
+    if (!eventOrInteractionIsEvent(child)) {
+      return false
+    }
+    try {
+      return getEventWithIdentifier(child, identifier.slice(1))
+    } catch (error) {
+      return false
+    }
+  })
+  // Return a matching child
+  const child = <Event>children.find(Boolean)
+  if (child) {
+    console.log("Found a child with id", JSON.stringify(identifier[1]))
+    return child
+  }
+  console.log("Did not find a child with id", JSON.stringify(identifier[1]))
+  throw new Error("No children match the rest of the identifier")
 }
 
 /**
