7921078f9b9a52d19b98bb2eb66322253fcad4e9
Ross Williams <rossjrw@gmail.com>
2021-05-13 03:12:20 +0100

    Flatten events file proxies

--- src/editor/lib/eventsFilesystemProxy.ts
+++ src/editor/lib/eventsFilesystemProxy.ts
@@ -1,138 +1,47 @@
-import { Event } from "../types"
-import { createEventAt } from "./identifier"
+import { Event, EventsList } from "../types"
 
 /**
- * Creates a proxy for a given events directory, and binds it to the given
- * events object at the given identifier.
+ * Asynchronously creates a proxy for the events directory.
  *
  * The proxy here constructs a fake interactions tree and populates it with
  * the files it contains, and intercepts save requests and transmits them to the
  * file proxies.
  *
- * @param dirPath - The path of the directory that contains the target
- * directory.
- * @param dirName - The name of the target directory.
- * @param parentEvent - The event that will contain this event.
- * @param parentDoAfterCallback - A callback to fire to asynchronously update
- * the data store.
+ * @param events - The events list to store the events in.
  */
-export function createEventsDirProxy(
-  dirPath: string,
-  dirName: string,
-  parentEvent: Event,
-  parentDoAfterCallback: () => void
-): void {
-  console.log(
-    "Creating events dir proxy for",
-    `${dirPath}/${dirName}`,
-    "in",
-    parentEvent.id
-  )
-  window.fileReadWrite.readEventsDir.singleResponse((files) => {
-    console.log(
-      "Received dir read for",
-      `${dirPath}/${dirName}`,
-      "with",
-      files.length,
-      "files"
-    )
-    // Create the new event
-    let event: Event = {
-      id: dirName,
-      summary: `Events from ${dirPath}/${dirName}`,
-      interactions: [],
-    }
-    // Define a callback to be executed after the children are sorted
-    function doAfterCallback() {
-      // Put the event into a proxy
-      event = new Proxy(event, {})
-      console.log(
-        "About to push a dir event with id",
-        JSON.stringify(event.id),
-        "into a parent event with id",
-        JSON.stringify(parentEvent.id)
-      )
-      // Push this new event to the parent event
-      createEventAt(parentEvent, [], event, "append")
-      // Execute the parent's callback
-      parentDoAfterCallback()
-    }
+export function createEventsDirProxy(events: EventsList): void {
+  console.log("Creating events dir proxy")
+  window.fileReadWrite.readEventsDir.singleResponse((filePaths) => {
     // Construct and bind this event's children
-    files.forEach(([file, fileIsDirectory]) => {
-      if (fileIsDirectory) {
-        console.log(
-          "File with name",
-          file.name,
-          "is a directory, so creating a dir proxy in event with id",
-          JSON.stringify(event.id)
-        )
-        return createEventsDirProxy(
-          `${dirPath}/${dirName}`, // This should probably be path.join
-          file.name,
-          event,
-          doAfterCallback
-        )
-      }
-      console.log(
-        "File with name",
-        file.name,
-        "is not a directory, so creating a file proxy in event with id",
-        JSON.stringify(event.id)
-      )
-      return createEventFileProxy(
-        `${dirPath}/${dirName}`,
-        file.name,
-        event,
-        doAfterCallback
-      )
+    filePaths.forEach((filePath) => {
+      createEventFileProxy(filePath, events)
     })
+    // TODO The dir proxy probably will need to be responsible for correctly
+    //  handling outright event replacement events
+    //  e.g events[<string>] = <Event>
   })
-  console.log(
-    "Requesting dir read for path",
-    JSON.stringify(dirPath),
-    "and name",
-    JSON.stringify(dirName)
-  )
-  window.fileReadWrite.readEventsDir.send(dirPath, dirName)
+  window.fileReadWrite.readEventsDir.send()
+  // return new Proxy?
 }
 
 /**
- * Creates a proxy for a given events file and binds it to the given parent
- * event.
+ * Asynchronously creates a proxy for a given events file.
  *
  * The proxy here intercepts save requests and transmits them to the file.
  *
- * @param filePath - The path to the directory containing the events file.
- * @param fileName - The name of the events file.
- * @param parentEvent - The event that will contain this event.
- * @param parentDoAfterCallback - A callback to fire to asynchronously
- * update the
- * data store.
+ * @param filePath - The path to the events file, including the file name,
+ * relative to the events directory.
+ * @param events - The list of events to append this event to.
  */
-function createEventFileProxy(
-  filePath: string,
-  fileName: string,
-  parentEvent: Event,
-  parentDoAfterCallback: () => void
-): void {
-  console.log(
-    "Creating events file proxy for",
-    `${filePath}/${fileName}`,
-    "in",
-    parentEvent.id
-  )
+function createEventFileProxy(filePath: string, events: EventsList): void {
+  console.log("Creating events file proxy for", JSON.stringify(filePath))
   window.fileReadWrite.readEventsFile.singleResponse((eventFile) => {
-    console.log("Received file read for", `${filePath}/${fileName}`)
-    const event = new Proxy(<Event>JSON.parse(eventFile), {})
+    // TODO Are these responses guaranteed to happen in the right order?
+    //  Should I make the IPC channel names dependent on e.g. the path of
+    //  the file, so that each file has its own unique channel?
+    events[filePath] = new Proxy(<Event>JSON.parse(eventFile), {})
     // TODO Intercept save requests
-    createEventAt(parentEvent, [], event, "append")
-    parentDoAfterCallback()
   })
-  console.log(
-    "Requesting file read for path",
-    JSON.stringify(filePath),
-    "and name",
-    JSON.stringify(fileName)
-  )
-  window.fileReadWrite.readEventsFile.send(filePath, fileName)
+  console.log("Requesting file read for path", JSON.stringify(filePath))
+  window.fileReadWrite.readEventsFile.send(filePath)
 }
